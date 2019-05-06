34f5029d52cc6ce64cecdae642466208d63838d6
Phil Shaughnessy <philip.shaughnessy@gmail.com>
2019-05-06 10:50:34 -0400

    Add supervised_todo_cache folder

---
+++ supervised_todo_cache/lib/todo/database.ex
@@ -0,0 +1,42 @@
+defmodule Todo.Database do
+  use GenServer
+
+  @db_folder "./persist"
+  @workers 3
+
+  def start_link do
+    IO.puts("starting database pool")
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def store(key, data) do
+    key
+    |> choose_worker()
+    |> Todo.DatabaseWorker.store(key, data)
+  end
+
+  def get(key) do
+    key
+    |> choose_worker()
+    |> Todo.DatabaseWorker.get(key)
+  end
+
+  defp choose_worker(key), do: GenServer.call(__MODULE__, {:choose_worker, key})
+
+  def init(_) do
+    File.mkdir_p!(@db_folder)
+    {:ok, start_workers()}
+  end
+
+  def handle_call({:choose_worker, key}, _from, workers) do
+    worker_key = :erlang.phash2(key, @workers)
+    {:reply, Map.get(workers, worker_key), workers}
+  end
+
+  defp start_workers do
+    for index <- 1..@workers, into: %{} do
+      {:ok, worker_pid} = Todo.DatabaseWorker.start_link(@db_folder)
+      {index - 1, worker_pid}
+    end
+  end
+end
---
+++ supervised_todo_cache/lib/todo/server.ex
@@ -0,0 +1,72 @@
+defmodule Todo.Server do
+  @moduledoc """
+  GenServer for a Todo.List
+  """
+
+  use GenServer
+
+  def start_link(name) do
+    IO.puts("starting the todo list server")
+    GenServer.start_link(Todo.Server, name)
+  end
+
+  def add_entry(pid, new_entry) do
+    GenServer.cast(pid, {:add_entry, new_entry})
+  end
+
+  def entries(pid, date) do
+    GenServer.call(pid, {:entries, date})
+  end
+
+  def update_entry(pid, entry_id, updater_fun) do
+    GenServer.cast(pid, {:update_entry, entry_id, updater_fun})
+  end
+
+  def delete_entry(pid, entry_id) do
+    GenServer.cast(pid, {:delete_entry, entry_id})
+  end
+
+  @impl GenServer
+  def init(name) do
+    send(self(), :real_init)
+    {:ok, name}
+  end
+
+  @impl GenServer
+  def handle_info(:real_init, name) do
+    {:noreply, {name, Todo.Database.get(name) || Todo.List.new()}}
+  end
+
+  def handle_info(_msg, state), do: {:noreply, state}
+
+  @impl GenServer
+  def handle_call({:entries, date}, _from, {name, todo_list}) do
+    {
+      :reply,
+      Todo.List.entries(todo_list, date),
+      {name, todo_list}
+    }
+  end
+
+  @impl GenServer
+  def handle_cast({:add_entry, entry}, {name, todo_list}) do
+    result = Todo.List.add_entry(todo_list, entry)
+    Todo.Database.store(name, result)
+    {:noreply, {name, result}}
+  end
+
+  @impl GenServer
+  def handle_cast({:update_entry, entry_id, update_fun}, {name, todo_list}) do
+    result = Todo.List.update_entry(todo_list, entry_id, update_fun)
+    Todo.Database.store(name, result)
+    {:noreply, {name, result}}
+  end
+
+  @impl GenServer
+  def handle_cast({:delete_entry, entry_id}, {name, todo_list}) do
+    result = Todo.List.delete_entry(todo_list, entry_id)
+    Todo.Database.store(name, result)
+    {:noreply, {name, result}}
+  end
+end
+
