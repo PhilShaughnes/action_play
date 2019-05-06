6aeb8d92dbd2f21194477d0d0846d71a45f5bf03
Phil Shaughnessy <philip.shaughnessy@gmail.com>
2019-05-06 11:52:14 -0400

    update server_process

--- supervised_todo_cache/lib/todo/cache.ex
+++ supervised_todo_cache/lib/todo/cache.ex
@@ -24,8 +24,8 @@ defmodule Todo.Cache do
     GenServer.start_link(__MODULE__, nil, name: __MODULE__)
   end
 
-  def server_process(cache_pid, todo_list_name) do
-    GenServer.call(cache_pid, {:server_process, todo_list_name})
+  def server_process(todo_list_name) do
+    GenServer.call(__MODULE__, {:server_process, todo_list_name})
   end
 
 end
