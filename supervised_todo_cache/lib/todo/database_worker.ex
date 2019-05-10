80c2dc489c9a8f63db61f22f2127c58592c1fe65
Phil <Philip.Shaughnessy@gmail.com>
2019-05-10 08:54:02 -0400

    Upgrade worker and build ProcessRegistry

--- supervised_todo_cache/lib/todo/database_worker.ex
+++ supervised_todo_cache/lib/todo/database_worker.ex
@@ -1,12 +1,27 @@
 defmodule Todo.DatabaseWorker do
     use GenServer
 
-  def start_link(db_folder) do
-    IO.puts("starting db worker")
-    GenServer.start_link(__MODULE__, db_folder)
+  def start_link({db_folder, worker_id}) do
+    IO.puts("starting db worker #{worker_id}")
+
+    GenServer.start_link(
+      __MODULE__, 
+      db_folder, 
+      name: via_tuple(worker_id)
+    )
   end
-  def store(pid, key, data), do: GenServer.cast(pid, {:store, key, data})
-  def get(pid, key), do: GenServer.call(pid, {:get, key})
+
+  def store(worker_id, key, data) do
+    GenServer.cast(via_tuple(worker_id), {:store, key, data})
+  end
+
+  def get(worker_id, key) do
+    GenServer.call(via_tuple(worker_id), {:get, key})
+  end
+
+  defp via_tuple(worker_id) do
+   Todo.ProcessRegistry.via_tuple({__MODULE__, worker_id}) 
+  end
 
   def init(db_folder) do
     File.mkdir_p!(db_folder)
