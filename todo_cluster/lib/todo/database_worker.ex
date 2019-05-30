fa4100d0f7f29d7aa6a6139a9aad464f857fc4aa
Phil <Philip.Shaughnessy@gmail.com>
2019-05-30 07:33:09 -0400

    add cluster folder

---
+++ todo_cluster/lib/todo/database_worker.ex
@@ -0,0 +1,50 @@
+defmodule Todo.DatabaseWorker do
+    use GenServer
+
+  def start_link(db_folder) do
+    IO.puts("starting db worker")
+
+    GenServer.start_link(
+      __MODULE__,
+      db_folder
+    )
+  end
+
+  def store(pid, key, data) do
+    GenServer.cast(pid, {:store, key, data})
+  end
+
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key})
+  end
+
+  def init(db_folder) do
+    {:ok, db_folder}
+  end
+
+  def handle_cast({:store, key, data}, db_folder) do
+    file_persist(key, db_folder, data)
+    {:noreply, db_folder}
+  end
+
+  def handle_call({:get, key}, _from, db_folder) do
+    {:reply, file_read(key, db_folder), db_folder}
+  end
+
+  defp file_persist(key, db_folder, data) do
+    key
+    |> file_name(db_folder)
+    |> File.write!(:erlang.term_to_binary(data))
+  end
+
+  defp file_read(key, db_folder) do
+    case File.read(file_name(key, db_folder)) do
+      {:ok, contents} -> :erlang.binary_to_term(contents)
+      {:error, :enoent} -> nil
+    end
+  end
+
+  defp file_name(key, db_folder) do
+    Path.join(db_folder, to_string(key))
+  end
+end
---
+++ todo_cluster/lib/todo/server.ex
@@ -0,0 +1,85 @@
+defmodule Todo.Server do
+  @moduledoc """
+  GenServer for a Todo.List
+  """
+  use GenServer, restart: :temporary
+
+  @expiry_idle_timeout :timer.seconds(30)
+
+  def start_link(name) do
+    IO.puts("starting the todo list server")
+    GenServer.start_link(Todo.Server, name, name: via_tuple(name))
+  end
+
+  defp via_tuple(name) do
+    Todo.ProcessRegistry.via_tuple({__MODULE__, name})
+  end
+
+  def add_entry(pid, new_entry) do
+    GenServer.cast(pid, {:add_entry, new_entry})
+  end
+
+  def entries(pid, date) do
+    GenServer.call(pid, {:entries, date})
+  end
+
+  def update_entry(pid, entry_id, updater_fun) do
+    GenServer.cast(pid, {:update_entry, entry_id, updater_fun})
+  end
+
+  def delete_entry(pid, entry_id) do
+    GenServer.cast(pid, {:delete_entry, entry_id})
+  end
+
+  @impl GenServer
+  def init(name) do
+    send(self(), :real_init)
+    {:ok, name, @expiry_idle_timeout}
+  end
+
+  @impl GenServer
+  def handle_info(:real_init, name) do
+    {:noreply, {name, Todo.Database.get(name) || Todo.List.new()}, @expiry_idle_timeout}
+  end
+
+  @impl GenServer
+  def handle_info(:timeout, {name, todo_list}) do
+    IO.puts("Stopping to-do server for #{name}")
+    {:stop, :normal, {name, todo_list}}
+  end
+
+  @impl GenServer
+  def handle_info(_msg, state), do: {:noreply, state, @expiry_idle_timeout}
+
+  @impl GenServer
+  def handle_call({:entries, date}, _from, {name, todo_list}) do
+    {
+      :reply,
+      Todo.List.entries(todo_list, date),
+      {name, todo_list},
+      @expiry_idle_timeout
+    }
+  end
+
+  @impl GenServer
+  def handle_cast({:add_entry, entry}, {name, todo_list}) do
+    result = Todo.List.add_entry(todo_list, entry)
+    Todo.Database.store(name, result)
+    {:noreply, {name, result}, @expiry_idle_timeout}
+  end
+
+  @impl GenServer
+  def handle_cast({:update_entry, entry_id, update_fun}, {name, todo_list}) do
+    result = Todo.List.update_entry(todo_list, entry_id, update_fun)
+    Todo.Database.store(name, result)
+    {:noreply, {name, result}, @expiry_idle_timeout}
+  end
+
+  @impl GenServer
+  def handle_cast({:delete_entry, entry_id}, {name, todo_list}) do
+    result = Todo.List.delete_entry(todo_list, entry_id)
+    Todo.Database.store(name, result)
+    {:noreply, {name, result}, @expiry_idle_timeout}
+  end
+end
+
